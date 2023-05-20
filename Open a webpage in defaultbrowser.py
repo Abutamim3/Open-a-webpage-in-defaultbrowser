e8a8a55d36d9450b10a26bfcd2b2f5a4e7a29cb9
Abdul Rahman Al-Harbi <133687927+Abutamim3@users.noreply.github.com>
2023-05-21 02:25:44 +0300

    Open-a-webpage-in-defaultbrowser
    **Developed By  Abdul Rahman Al-Harbi**
    
    https://github.com/Abutamim3
    
    #THANK YOU FOR DOWNLOADING

---
+++ Open a webpage in defaultbrowser.py
@@ -0,0 +1,9 @@
+'''Open a website in default browser'''
+#Developed by Abdul Rahman Al-Harbi
+
+import webbrowser
+
+# Replace google.com with the website you want.
+webbrowser.open('https://www.youtube.com/channel/UC3P0TStz00vTaBfJk1oR88A')
+
+print("Developed by Abdul Rahman Al-Harbi")
